16e43c0eb470cd3c87cc6204f6f60a42dfd7c0a5
Alina Gainanova <alinagainanova@MacBook-Pro-Alina.local>
2020-10-26 00:48:44 +0300

    feat: add project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+    const ones = ['','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tenner = ['ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number == 0) {
+        result = 'zero';
+    } else if (number < 10) {
+        result = ones[number];
+    } else if (number < 20) {
+        result = teens[number-10];
+    } else if (number < 100) {
+        result = tenner[String(number)[0]-1] + ' ' + ones[String(number)[1]];
+    } else {
+        result = ones[String(number)[0]] + ' hundred ' + ((number%100 == 0) ? '' : toReadable(number%100));
+    }
+
+
+
+    return result.trim()
 }
